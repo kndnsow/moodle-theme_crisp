22d1cedc78c84d7739970b92f7dd6e5a43d35765
parthajeet <parthajeet@dualcube.com>
2018-02-26 16:21:26 +0530

    version update

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015081706;
+$plugin->version   = 2015081707;
 $plugin->component = 'theme_crisp';
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2013050100,
